6291eb41b934dbd2f12cf5cb6a37dfedcefde70a
Mike Chubey <mike.chubey2@gmail.com>
2015-12-06 19:25:34 -0700

    Switched to C++

---
+++ Base.cpp
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#define PI					3.14159265358979
+
+/*  Test tone frequency in Hz  */
+#define FREQUENCY			440.0
+
+/*  Test tone duration in seconds  */
+#define DURATION			2.0				
+
+/*  Standard sample rate in Hz  */
+#define SAMPLE_RATE			44100.0
+
+/*  Standard sample size in bits  */
+#define BITS_PER_SAMPLE		16
+
+/*  Standard sample size in bytes  */
+#define BYTES_PER_SAMPLE	(BITS_PER_SAMPLE/8)
+
+/*  Number of channels  */
+#define MONOPHONIC			1
+//#define STEREOPHONIC		2
+
+
+void writeWaveFileHeader(int channels, int numberSamples, int bitsPerSample,
+double outputRate, FILE *outputFile);
+size_t fwriteIntLSB(int data, FILE *stream);
+size_t fwriteShortLSB(short int data, FILE *stream);
+void readInput(char *filename);
+void convolve(short x[], int N, float h[], int M, short y[], int P);
+
+int 	myChunkSize;
+int		mySubChunk1Size;
+short 	myFormat;
+short 	myChannels;
+int   	mySampleRate;
+int   	myByteRate;
+short 	myBlockAlign;
+short 	myBitsPerSample;
+int		myDataSize;
+char* 	myData;
+
+int main(int argc, char *argv[]) {
+	
+
+	char* inputFile = NULL;
+	char* IRFile = NULL;
+	char* outputFilename = NULL;
+	if (argc == 4) {
+		
+	
+	}
+	public void readInput(char* fileName) {
+		ifstream inFile(filename, ios::in | ios::binary);
+
+		printf("Reading wav file...\n");
+
+		inFile.seekg(4, ios::beg);
+		inFile.read((char*)&myChunkSize, 4); // read the ChunkSize
+
+		inFile.seekg(16, ios::beg);
+		inFile.read((char*)&mySubChunk1Size, 4); // read the SubChunk1Size
+
+		inFile.seekg(20, ios::beg);
+		inFile.read((char*)&myFormat, sizeof(short)); // read the file format.  This should be 1 for PCM
+
+													  //inFile.seekg(22, ios::beg);
+		inFile.read((char*)&myChannels, sizeof(short)); // read the # of channels (1 or 2)
+
+														//inFile.seekg(24, ios::beg);
+		inFile.read((char*)&mySampleRate, sizeof(int)); // read the samplerate
+
+														//inFile.seekg(28, ios::beg);
+		inFile.read((char*)&myByteRate, sizeof(int)); // read the byterate
+
+													  //inFile.seekg(32, ios::beg);
+		inFile.read((char*)&myBlockAlign, sizeof(short)); // read the blockalign
+
+														  //inFile.seekg(34, ios::beg);
+		inFile.read((char*)&myBitsPerSample, sizeof(short)); // read the bitspersample
+
+		inFile.seekg(40, ios::beg);
+		inFile.read((char*)&myDataSize, sizeof(int)); // read the size of the data
+
+
+													  // read the data chunk
+		myData = new char[myDataSize];
+		inFile.seekg(44, ios::beg);
+		inFile.read(myData, myDataSize);
+
+		inFile.close(); // close the input file
+
+		my_signal = NULL;
+
+		if (myBitsPerSample == 8)
+		{
+			mySignalSize = myDataSize;
+			my_signal = new short[mySignalSize];
+			for (int i = 0; i < myDataSize; i++)
+				my_signal[i] = (short)((unsigned char)myData[i]);
+
+		}
+		else{
+			mySignalSize = myDataSize / 2;
+			my_signal = new short[mySignalSize];
+			short val;
+			for (int i = 0; i < myDataSize; i += 2)
+			{
+				val = (short)((unsigned char)myData[i]);
+				val += (short)((unsigned char)myData[i + 1]) * 256;
+				my_signal[i / 2] = val;
+			}
+		}
+	}
+
+}
+
+
